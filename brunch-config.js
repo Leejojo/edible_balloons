ce3ec8ab1c36f1eb6d5a1dd7a97143ac09b5f7c8
Leejojo <noodle.lee@gmail.com>
2016-09-01 09:22:41 -0400

    adds brunch-sass-bootstrap

---
+++ brunch-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'vendor.js': /^(?!app)/,
+        'app.js': /^app/
+      }
+    },
+    stylesheets: {
+      joinTo: 'app.css'
+    }
+  },
+
+  plugins: {
+    babel: {presets: ['es2015']},
+    sass: {
+      options: {
+        includePaths: ['node_modules/bootstrap/scss']
+      }
+    }
+  }
+};
